aff9384f9f7ce701def6199d4d17d818941ebfb2
cristihan <schweizercristian@gmail.com>
2022-12-12 11:16:35 +0100

    Communication to Redis

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,12 +1,21 @@
 import { Controller, Get } from '@nestjs/common';
 import { AppService } from './app.service';
+import { EventPattern, MessagePattern } from '@nestjs/microservices';
 
 @Controller()
 export class AppController {
   constructor(private readonly appService: AppService) {}
 
-  @Get('/test')
+  @Get('/health')
   getHello(): string {
-    return this.appService.getHello();
+    return this.appService.checkHealth();
   }
+
+  @Get('/testEvents')
+  emitTestEvent(): void {
+    console.log('testEvents controller')
+    this.appService.emitTestEvent();
+  }
+
+
 }
--- src/app.service.ts
+++ src/app.service.ts
@@ -1,8 +1,25 @@
 import { Injectable } from '@nestjs/common';
+import { ClientProxy, ClientProxyFactory, Transport } from '@nestjs/microservices';
 
 @Injectable()
 export class AppService {
-  getHello(): string {
-    return 'Hello World!';
+  private client: ClientProxy
+
+  constructor() {
+    this.client = ClientProxyFactory.create({
+      transport: Transport.REDIS,
+      options: {
+        host: 'redis',
+        port: 6379        
+      }
+    })
+  }
+
+  checkHealth(): string {
+    return 'OK!';
+  }
+
+  emitTestEvent(): void {
+    this.client.emit<number>('user_created', {user: "pepe", id: "1"});
   }
 }
